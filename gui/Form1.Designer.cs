9cc6d17240f218acbc544ef885c1355bff92d4bc
ShadowCrusader <lukerackham94@gmail.com>
2025-06-05 17:28:55 -0600

    Skeleton
    Upload and parse tab updated, with working upload bar, drag/drop feature added and date selection to help with library sorting

--- gui/Form1.Designer.cs
+++ gui/Form1.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.lblFileStatus = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -52,13 +68,148 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.AllowDrop = true;
+            this.tabPage1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.tabPage1.Controls.Add(this.lblFileStatus);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.checkBox3);
+            this.tabPage1.Controls.Add(this.checkBox2);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(971, 623);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Upload/Parse";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            this.tabPage1.DragDrop += new System.Windows.Forms.DragEventHandler(this.tabPage1_DragDrop);
+            this.tabPage1.DragEnter += new System.Windows.Forms.DragEventHandler(this.tabPage1_DragEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Actual Scan Date:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(261, 287);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(247, 22);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(68, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(392, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(400, 457);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Uploading Data...";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(261, 431);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(417, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(394, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Upload Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(578, 376);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(170, 20);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Save Data to Database";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(380, 376);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(154, 20);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Scrub Sensitive Data";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(191, 376);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Parse Immediately";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(754, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(261, 253);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(487, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select files to upload (.zip, .txt files)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // tabPage2
             // 
@@ -67,7 +218,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(971, 623);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Data Omission";
+            this.tabPage2.Text = "Dataset Library";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
@@ -77,7 +228,7 @@
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage3.Size = new System.Drawing.Size(971, 623);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Dataset Library";
+            this.tabPage3.Text = "Data Omission";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
@@ -100,16 +251,32 @@
             this.tabPage5.Text = "Settings";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
+            // lblFileStatus
+            // 
+            this.lblFileStatus.AutoSize = true;
+            this.lblFileStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFileStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblFileStatus.Location = new System.Drawing.Point(548, 289);
+            this.lblFileStatus.Name = "lblFileStatus";
+            this.lblFileStatus.Size = new System.Drawing.Size(158, 20);
+            this.lblFileStatus.TabIndex = 12;
+            this.lblFileStatus.Text = "No Files Selected";
+            this.lblFileStatus.Click += new System.EventHandler(this.label4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(982, 653);
             this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Netra";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,6 +289,19 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFileStatus;
     }
 }
 
--- gui/Form1.cs
+++ gui/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Netra
 {
@@ -15,6 +17,390 @@ namespace Netra
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create an OpenFileDialog to let user select files
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the file filter to only show .zip and .txt files
+            openFileDialog.Filter = "Scan Files|*.zip;*.txt|All Files|*.*";
+            openFileDialog.Title = "Select Scan Files";
+
+            // Allow multiple file selection
+            openFileDialog.Multiselect = true;
+
+            // Show the dialog and check if user clicked OK
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get all selected file paths
+                string[] selectedFiles = openFileDialog.FileNames;
+
+                // Validate file types
+                List<string> validFiles = new List<string>();
+                List<string> invalidFiles = new List<string>();
+
+                foreach (string file in selectedFiles)
+                {
+                    string extension = Path.GetExtension(file).ToLower();
+                    if (extension == ".zip" || extension == ".txt")
+                    {
+                        validFiles.Add(file);
+                    }
+                    else
+                    {
+                        invalidFiles.Add(file);
+                    }
+                }
+
+                // Update the textbox based on results
+                if (validFiles.Count == 1)
+                {
+                    textBox1.Text = validFiles[0]; // Show single file path
+                }
+                else if (validFiles.Count > 1)
+                {
+                    textBox1.Text = $"{validFiles.Count} files selected"; // Show count for multiple files
+                }
+
+                // Update status label based on validation results
+                if (invalidFiles.Count > 0 && validFiles.Count == 0)
+                {
+                    // All files are invalid
+                    lblFileStatus.Text = "✗ Invalid file types detected";
+                    lblFileStatus.ForeColor = Color.Red;
+                    textBox1.Text = "No valid files selected";
+                }
+                else if (invalidFiles.Count > 0 && validFiles.Count > 0)
+                {
+                    // Some valid, some invalid
+                    lblFileStatus.Text = $"⚠ {validFiles.Count} valid files, {invalidFiles.Count} ignored";
+                    lblFileStatus.ForeColor = Color.Orange;
+                }
+                else if (validFiles.Count > 0)
+                {
+                    // All files are valid
+                    lblFileStatus.Text = $"✓ {validFiles.Count} files ready to upload";
+                    lblFileStatus.ForeColor = Color.Green;
+                }
+            }
+            else
+            {
+                // User cancelled the dialog
+                lblFileStatus.Text = "No files selected";
+                lblFileStatus.ForeColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check if user has selected files
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please select files first!", "No Files Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Show the progress bar and status label
+            progressBar1.Visible = true;
+            label2.Visible = true;
+
+            // Reset progress bar
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 100;
+
+            // Disable the upload button during processing
+            button2.Enabled = false;
+
+            // Start the upload simulation
+            SimulateUpload();
+        }
+
+        private async void SimulateUpload()
+        {
+            try
+            {
+                // Phase 1: Uploading files
+                label2.Text = "Uploading files...";
+                for (int i = 0; i <= 40; i++)
+                {
+                    progressBar1.Value = i;
+                    await Task.Delay(50); // Simulate time
+                    Application.DoEvents(); // Keep UI responsive
+                }
+
+                // Phase 2: Processing files
+                label2.Text = "Processing scan data...";
+                for (int i = 41; i <= 80; i++)
+                {
+                    progressBar1.Value = i;
+                    await Task.Delay(30);
+                    Application.DoEvents();
+                }
+
+                // Phase 3: Saving to database
+                label2.Text = "Saving to database...";
+                for (int i = 81; i <= 100; i++)
+                {
+                    progressBar1.Value = i;
+                    await Task.Delay(40);
+                    Application.DoEvents();
+                }
+
+                // Complete!
+                label2.Text = "Upload complete!";
+                MessageBox.Show("Files uploaded and processed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Reset UI
+                progressBar1.Visible = false;
+                label2.Visible = false;
+                button2.Enabled = true;
+                textBox1.Text = "";
+
+                lblFileStatus.Text = "No files selected";
+                lblFileStatus.ForeColor = Color.Red;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during upload: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button2.Enabled = true;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_2(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            // Check if the dragged data contains files
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Get the file paths
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Check if at least one file has a valid extension
+                bool hasValidFile = false;
+                foreach (string file in files)
+                {
+                    string extension = Path.GetExtension(file).ToLower();
+                    if (extension == ".zip" || extension == ".txt")
+                    {
+                        hasValidFile = true;
+                        break;
+                    }
+                }
+
+                // Allow drop if we have valid files
+                if (hasValidFile)
+                {
+                    e.Effect = DragDropEffects.Copy; // Show "copy" cursor
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None; // Show "no drop" cursor
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None; // Not files, don't allow
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            // Get the dropped files
+            string[] droppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // Validate file types (same logic as Browse button)
+            List<string> validFiles = new List<string>();
+            List<string> invalidFiles = new List<string>();
+
+            foreach (string file in droppedFiles)
+            {
+                string extension = Path.GetExtension(file).ToLower();
+                if (extension == ".zip" || extension == ".txt")
+                {
+                    validFiles.Add(file);
+                }
+                else
+                {
+                    invalidFiles.Add(file);
+                }
+            }
+
+            // Update the textbox based on results
+            if (validFiles.Count == 1)
+            {
+                textBox1.Text = validFiles[0]; // Show single file path
+            }
+            else if (validFiles.Count > 1)
+            {
+                textBox1.Text = $"{validFiles.Count} files selected"; // Show count for multiple files
+            }
+
+            // Update status label based on validation results
+            if (invalidFiles.Count > 0 && validFiles.Count == 0)
+            {
+                // All files are invalid
+                lblFileStatus.Text = "✗ Invalid file types detected";
+                lblFileStatus.ForeColor = Color.Red;
+                textBox1.Text = "No valid files selected";
+            }
+            else if (invalidFiles.Count > 0 && validFiles.Count > 0)
+            {
+                // Some valid, some invalid
+                lblFileStatus.Text = $"⚠ {validFiles.Count} valid files, {invalidFiles.Count} ignored";
+                lblFileStatus.ForeColor = Color.Orange;
+            }
+            else if (validFiles.Count > 0)
+            {
+                // All files are valid
+                lblFileStatus.Text = $"✓ {validFiles.Count} files ready to upload";
+                lblFileStatus.ForeColor = Color.Green;
+            }
+        }
+
+        private void tabPage1_DragDrop(object sender, DragEventArgs e)
+        {
+            // Get the dropped files
+            string[] droppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // Validate file types (same logic as Browse button)
+            List<string> validFiles = new List<string>();
+            List<string> invalidFiles = new List<string>();
+
+            foreach (string file in droppedFiles)
+            {
+                string extension = Path.GetExtension(file).ToLower();
+                if (extension == ".zip" || extension == ".txt")
+                {
+                    validFiles.Add(file);
+                }
+                else
+                {
+                    invalidFiles.Add(file);
+                }
+            }
+
+            // Update the textbox based on results
+            if (validFiles.Count == 1)
+            {
+                textBox1.Text = validFiles[0]; // Show single file path
+            }
+            else if (validFiles.Count > 1)
+            {
+                textBox1.Text = $"{validFiles.Count} files selected"; // Show count for multiple files
+            }
+
+            // Update status label based on validation results
+            if (invalidFiles.Count > 0 && validFiles.Count == 0)
+            {
+                // All files are invalid
+                lblFileStatus.Text = "✗ Invalid file types detected";
+                lblFileStatus.ForeColor = Color.Red;
+                textBox1.Text = "No valid files selected";
+            }
+            else if (invalidFiles.Count > 0 && validFiles.Count > 0)
+            {
+                // Some valid, some invalid
+                lblFileStatus.Text = $"⚠ {validFiles.Count} valid files, {invalidFiles.Count} ignored";
+                lblFileStatus.ForeColor = Color.Orange;
+            }
+            else if (validFiles.Count > 0)
+            {
+                // All files are valid
+                lblFileStatus.Text = $"✓ {validFiles.Count} files ready to upload";
+                lblFileStatus.ForeColor = Color.Green;
+            }
+        }
+
+        private void tabPage1_DragEnter(object sender, DragEventArgs e)
+        {
+            // Check if the dragged data contains files
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Get the file paths
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Check if at least one file has a valid extension
+                bool hasValidFile = false;
+                foreach (string file in files)
+                {
+                    string extension = Path.GetExtension(file).ToLower();
+                    if (extension == ".zip" || extension == ".txt")
+                    {
+                        hasValidFile = true;
+                        break;
+                    }
+                }
+
+                // Allow drop if we have valid files
+                if (hasValidFile)
+                {
+                    e.Effect = DragDropEffects.Copy; // Show "copy" cursor
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None; // Show "no drop" cursor
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None; // Not files, don't allow
+            }
         }
     }
 }
